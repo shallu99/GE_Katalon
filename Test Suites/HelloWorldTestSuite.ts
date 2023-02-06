1ab5ecebc7c2d22145a2983da9a6485d2ae4dd1e
VersionControlWS <versioncontrolws@gmail.com>
2023-02-06 14:29:57 +0530

    Only 3 files added for jenkins

---
+++ Test Suites/HelloWorldTestSuite.ts
@@ -0,0 +1,21 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>HelloWorldTestSuite</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>9a89a9e5-9fad-40f2-aad8-1d803fefa33e</testSuiteGuid>
+   <testCaseLink>
+      <guid>5b5fdfb4-1b04-41a3-a5a7-da724bf70702</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Main TestCases/Test_Print</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
